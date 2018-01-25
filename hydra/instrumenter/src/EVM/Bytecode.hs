92a2b2e996ab3a793ef9a4300485f141cfcaa16f
Lorenz Breidenbach <lorenzb@eth.edu>
2018-01-25 16:44:44 +0100

    Byzantium opcodes

--- hydra/instrumenter/src/EVM/Bytecode.hs
+++ hydra/instrumenter/src/EVM/Bytecode.hs
@@ -61,6 +61,8 @@ data Opcode = STOP
             | GASPRICE
             | EXTCODESIZE
             | EXTCODECOPY
+            | RETURNDATASIZE
+            | RETURNDATACOPY
             | BLOCKHASH
             | COINBASE
             | TIMESTAMP
@@ -92,6 +94,8 @@ data Opcode = STOP
             | CALLCODE
             | RETURN
             | DELEGATECALL
+            | STATICCALL
+            | REVERT
             | SUICIDE
             | Unknown Word8
             deriving (Show, Eq)
@@ -142,6 +146,8 @@ word8ToOpcode 0x39 = CODECOPY
 word8ToOpcode 0x3a = GASPRICE
 word8ToOpcode 0x3b = EXTCODESIZE
 word8ToOpcode 0x3c = EXTCODECOPY
+word8ToOpcode 0x3d = RETURNDATASIZE
+word8ToOpcode 0x3e = RETURNDATACOPY
 word8ToOpcode 0x40 = BLOCKHASH
 word8ToOpcode 0x41 = COINBASE
 word8ToOpcode 0x42 = TIMESTAMP
@@ -170,6 +176,8 @@ word8ToOpcode 0xf1 = CALL
 word8ToOpcode 0xf2 = CALLCODE
 word8ToOpcode 0xf3 = RETURN
 word8ToOpcode 0xf4 = DELEGATECALL
+word8ToOpcode 0xfa = STATICCALL
+word8ToOpcode 0xfd = REVERT
 word8ToOpcode 0xff = SUICIDE
 word8ToOpcode w | 0x60 <= w && w < 0x60 + 32 = PUSH (fromIntegral $ w - 0x60 + 1) (2^256)
                 | 0x80 <= w && w < 0x80 + 16 = DUP (fromIntegral $ w - 0x80 + 1)
@@ -177,76 +185,80 @@ word8ToOpcode w | 0x60 <= w && w < 0x60 + 32 = PUSH (fromIntegral $ w - 0x60 + 1
                 | otherwise                  = Unknown w
 
 opcodeToWord8 :: Opcode -> Word8
-opcodeToWord8 STOP         = 0x00
-opcodeToWord8 ADD          = 0x01
-opcodeToWord8 MUL          = 0x02
-opcodeToWord8 SUB          = 0x03
-opcodeToWord8 DIV          = 0x04
-opcodeToWord8 SDIV         = 0x05
-opcodeToWord8 MOD          = 0x06
-opcodeToWord8 SMOD         = 0x07
-opcodeToWord8 ADDMOD       = 0x08
-opcodeToWord8 MULMOD       = 0x09
-opcodeToWord8 EXP          = 0x0a
-opcodeToWord8 SIGNEXTEND   = 0x0b
-opcodeToWord8 LT           = 0x10
-opcodeToWord8 GT           = 0x11
-opcodeToWord8 SLT          = 0x12
-opcodeToWord8 SGT          = 0x13
-opcodeToWord8 EQ           = 0x14
-opcodeToWord8 ISZERO       = 0x15
-opcodeToWord8 AND          = 0x16
-opcodeToWord8 OR           = 0x17
-opcodeToWord8 XOR          = 0x18
-opcodeToWord8 NOT          = 0x19
-opcodeToWord8 BYTE         = 0x1a
-opcodeToWord8 SHA3         = 0x20
-opcodeToWord8 ADDRESS      = 0x30
-opcodeToWord8 BALANCE      = 0x31
-opcodeToWord8 ORIGIN       = 0x32
-opcodeToWord8 CALLER       = 0x33
-opcodeToWord8 CALLVALUE    = 0x34
-opcodeToWord8 CALLDATALOAD = 0x35
-opcodeToWord8 CALLDATASIZE = 0x36
-opcodeToWord8 CALLDATACOPY = 0x37
-opcodeToWord8 CODESIZE     = 0x38
-opcodeToWord8 CODECOPY     = 0x39
-opcodeToWord8 GASPRICE     = 0x3a
-opcodeToWord8 EXTCODESIZE  = 0x3b
-opcodeToWord8 EXTCODECOPY  = 0x3c
-opcodeToWord8 BLOCKHASH    = 0x40
-opcodeToWord8 COINBASE     = 0x41
-opcodeToWord8 TIMESTAMP    = 0x42
-opcodeToWord8 NUMBER       = 0x43
-opcodeToWord8 DIFFICULTY   = 0x44
-opcodeToWord8 GASLIMIT     = 0x45
-opcodeToWord8 POP          = 0x50
-opcodeToWord8 MLOAD        = 0x51
-opcodeToWord8 MSTORE       = 0x52
-opcodeToWord8 MSTORE8      = 0x53
-opcodeToWord8 SLOAD        = 0x54
-opcodeToWord8 SSTORE       = 0x55
-opcodeToWord8 JUMP         = 0x56
-opcodeToWord8 JUMPI        = 0x57
-opcodeToWord8 PC           = 0x58
-opcodeToWord8 MSIZE        = 0x59
-opcodeToWord8 GAS          = 0x5a
-opcodeToWord8 JUMPDEST     = 0x5b
-opcodeToWord8 (PUSH n _)   = 0x60 - 1 + n
-opcodeToWord8 (DUP n )     = 0x80 - 1 + n
-opcodeToWord8 (SWAP n )    = 0x90 - 1 + n
-opcodeToWord8 LOG0         = 0xa0
-opcodeToWord8 LOG1         = 0xa1
-opcodeToWord8 LOG2         = 0xa2
-opcodeToWord8 LOG3         = 0xa3
-opcodeToWord8 LOG4         = 0xa4
-opcodeToWord8 CREATE       = 0xf0
-opcodeToWord8 CALL         = 0xf1
-opcodeToWord8 CALLCODE     = 0xf2
-opcodeToWord8 RETURN       = 0xf3
-opcodeToWord8 DELEGATECALL = 0xf4
-opcodeToWord8 SUICIDE      = 0xff
-opcodeToWord8 (Unknown x)  = x
+opcodeToWord8 STOP           = 0x00
+opcodeToWord8 ADD            = 0x01
+opcodeToWord8 MUL            = 0x02
+opcodeToWord8 SUB            = 0x03
+opcodeToWord8 DIV            = 0x04
+opcodeToWord8 SDIV           = 0x05
+opcodeToWord8 MOD            = 0x06
+opcodeToWord8 SMOD           = 0x07
+opcodeToWord8 ADDMOD         = 0x08
+opcodeToWord8 MULMOD         = 0x09
+opcodeToWord8 EXP            = 0x0a
+opcodeToWord8 SIGNEXTEND     = 0x0b
+opcodeToWord8 LT             = 0x10
+opcodeToWord8 GT             = 0x11
+opcodeToWord8 SLT            = 0x12
+opcodeToWord8 SGT            = 0x13
+opcodeToWord8 EQ             = 0x14
+opcodeToWord8 ISZERO         = 0x15
+opcodeToWord8 AND            = 0x16
+opcodeToWord8 OR             = 0x17
+opcodeToWord8 XOR            = 0x18
+opcodeToWord8 NOT            = 0x19
+opcodeToWord8 BYTE           = 0x1a
+opcodeToWord8 SHA3           = 0x20
+opcodeToWord8 ADDRESS        = 0x30
+opcodeToWord8 BALANCE        = 0x31
+opcodeToWord8 ORIGIN         = 0x32
+opcodeToWord8 CALLER         = 0x33
+opcodeToWord8 CALLVALUE      = 0x34
+opcodeToWord8 CALLDATALOAD   = 0x35
+opcodeToWord8 CALLDATASIZE   = 0x36
+opcodeToWord8 CALLDATACOPY   = 0x37
+opcodeToWord8 CODESIZE       = 0x38
+opcodeToWord8 CODECOPY       = 0x39
+opcodeToWord8 GASPRICE       = 0x3a
+opcodeToWord8 EXTCODESIZE    = 0x3b
+opcodeToWord8 EXTCODECOPY    = 0x3c
+opcodeToWord8 RETURNDATASIZE = 0x3d 
+opcodeToWord8 RETURNDATACOPY = 0x3e 
+opcodeToWord8 BLOCKHASH      = 0x40
+opcodeToWord8 COINBASE       = 0x41
+opcodeToWord8 TIMESTAMP      = 0x42
+opcodeToWord8 NUMBER         = 0x43
+opcodeToWord8 DIFFICULTY     = 0x44
+opcodeToWord8 GASLIMIT       = 0x45
+opcodeToWord8 POP            = 0x50
+opcodeToWord8 MLOAD          = 0x51
+opcodeToWord8 MSTORE         = 0x52
+opcodeToWord8 MSTORE8        = 0x53
+opcodeToWord8 SLOAD          = 0x54
+opcodeToWord8 SSTORE         = 0x55
+opcodeToWord8 JUMP           = 0x56
+opcodeToWord8 JUMPI          = 0x57
+opcodeToWord8 PC             = 0x58
+opcodeToWord8 MSIZE          = 0x59
+opcodeToWord8 GAS            = 0x5a
+opcodeToWord8 JUMPDEST       = 0x5b
+opcodeToWord8 (PUSH n _)     = 0x60 - 1 + n
+opcodeToWord8 (DUP n )       = 0x80 - 1 + n
+opcodeToWord8 (SWAP n )      = 0x90 - 1 + n
+opcodeToWord8 LOG0           = 0xa0
+opcodeToWord8 LOG1           = 0xa1
+opcodeToWord8 LOG2           = 0xa2
+opcodeToWord8 LOG3           = 0xa3
+opcodeToWord8 LOG4           = 0xa4
+opcodeToWord8 CREATE         = 0xf0
+opcodeToWord8 CALL           = 0xf1
+opcodeToWord8 CALLCODE       = 0xf2
+opcodeToWord8 RETURN         = 0xf3
+opcodeToWord8 DELEGATECALL   = 0xf4
+opcodeToWord8 STATICCALL     = 0xfa
+opcodeToWord8 REVERT         = 0xfd
+opcodeToWord8 SUICIDE        = 0xff
+opcodeToWord8 (Unknown x)    = x
 
 bytestringToInteger :: ByteString -> Integer
 bytestringToInteger b = sum $ zipWith (*) (map fromIntegral . reverse . unpack $ b) (iterate (* 256) 1)
