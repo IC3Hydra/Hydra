3874d790e30aa608bd4272f634db8a11fc0b7803
Philip Daian <phil@linux.com>
2018-02-07 21:27:27 +0100

    global cache for instrumenter test to allow for smaller batches, nicer progress display

--- hydra/test/test_instrumenter_coverage.py
+++ hydra/test/test_instrumenter_coverage.py
@@ -9,10 +9,13 @@ import os
 import web3
 w3 = web3.Web3(web3.IPCProvider('/home/debian/geth_temp_fast/geth.ipc'))
 
-lock = threading.Lock()
+latest_block_lock = threading.Lock()
 latest_block = -1
 
-def crawl(start_block, end_block, blockpath):
+results_cache_lock = threading.Lock()
+results_cache = {}
+
+def crawl(start_block, end_block, blockpath, thread_id):
 
     block_file_out_path = blockpath + os.sep + str(start_block) + "_" + str(end_block)
 
@@ -35,32 +38,40 @@ def crawl(start_block, end_block, blockpath):
 
             code = w3.eth.getCode(addr)
             if code != "0x":
-                if (code,) not in unique_codes:
+                if (code,) not in results_cache:
                     instrumented_code = dep.instrument_head(True, code[2:], None, MC_address, run_constructor=False, detect_swarm=True, write_stderr_to=block_file_out_path + ".errors")
+                    results_cache_lock.acquire()
                     if len(instrumented_code) == 0:
-                        unique_codes[(code,)] = (1, False)
+                        results_cache[(code,)] = False
                     else:
+                        results_cache[(code,)] = True
+                    results_cache_lock.release()
+                result = results_cache[(code,)]
+                if (code,) not in unique_codes:
+                    if result:
                         unique_codes[(code,)] = (1, True)
                         instrumentable += 1
+                    else:
+                        unique_codes[(code,)] = (1, False)
 
                 else:
                     (count, status) = unique_codes[(code,)]
                     unique_codes[(code,)] = (count+1, status)
-        outsummary = "block {} ({}/{} instrumented)\n".format(i, instrumentable, len(unique_codes))
+        outsummary = "block {} ({}/{} instrumented [thread {}, {}% complete])".format(i, instrumentable, len(unique_codes), thread_id, ((float(i) - start_block) * 100) / (end_block - start_block))
         print(outsummary)
     open(block_file_out_path, "w").write(str(unique_codes))
 
 
-def do_crawl(blockspath, batchsize):
+def do_crawl(blockspath, batchsize, thread_id):
     global latest_block
     while True:
         # lock and grab a batch
         # run the batch
-        lock.acquire()
+        latest_block_lock.acquire()
         last_block = latest_block
         latest_block -= batchsize
-        lock.release()
-        crawl(last_block - batchsize + 1, last_block, blockspath)
+        latest_block_lock.release()
+        crawl(last_block - batchsize + 1, last_block, blockspath, thread_id)
 
 
 if __name__ == "__main__":
@@ -69,14 +80,14 @@ if __name__ == "__main__":
     parser.add_argument("end", type=int, help="end block")
     parser.add_argument("blockspath", type=str, help="block data files prefix")
     parser.add_argument("numthreads", type=int, help="number of threads to use")
-    parser.add_argument("batchsize", type=int, help="number of blocks to batch together (higher=more caching, slower writes to disk)")
+    parser.add_argument("batchsize", type=int, help="number of blocks to batch together (higher=larger files, slower writes to disk)")
 
     args = parser.parse_args()
 
     latest_block = args.end
 
     for i in range(args.numthreads):
-        threading.Thread(target=do_crawl, args=[args.blockspath, args.batchsize], daemon=True).start()
+        threading.Thread(target=do_crawl, args=[args.blockspath, args.batchsize, i], daemon=True).start()
 
     while latest_block > 0: # nasty polling loop
         time.sleep(5)
