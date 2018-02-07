af63fe6d54d347d14def20df05a7d12dd12681ab
Florian Tramer <florian.tramer@gmail.com>
2018-02-07 08:31:19 -0800

    use web3 in coverage test

--- hydra/test/test_instrumenter_coverage.py
+++ hydra/test/test_instrumenter_coverage.py
@@ -1,47 +1,10 @@
-import requests
 from utils.pyethereum_test_utils import PyEthereumHydraDeployment
 from ethereum import utils
 from ethereum.tools import tester
-import time
 import subprocess
 
-URL = 'https://mainnet.infura.io/'
-#URL = 'http://localhost:8545/'
-
-
-def get_block(block_id):
-
-    data = '{' \
-           '"jsonrpc":"2.0", ' \
-           '"method":"eth_getBlockByNumber", ' \
-           '"params":["' + '0x{:x}'.format(block_id) + '", true], ' \
-           '"id":1' \
-           '}'
-
-    #time.sleep(0.05)
-    r = requests.post(URL, data=data)
-    try:
-        return r.json()['result']
-    except KeyError:
-        print(r.json())
-        return r.json()['result']
-
-
-def get_code(addr):
-    data = '{' \
-           '"jsonrpc":"2.0", ' \
-           '"method":"eth_getCode", ' \
-           '"params":["' + addr + '", "latest"], ' \
-           '"id":1' \
-           '}'
-
-    #time.sleep(0.05)
-    r = requests.post(URL, data=data)
-    try:
-        return r.json()['result']
-    except KeyError:
-        print(r.json())
-        return "0x"
+import web3
+w3 = web3.Web3(web3.IPCProvider('/Users/floriantramer/Library/Ethereum/geth.ipc'))
 
 
 def crawl(end_block):
@@ -61,15 +24,20 @@ def crawl(end_block):
     for i in range(end_block, 0, -1):
         print("block {} ({}/{} unique instrumented, {}/{} total instrumented)".format(i, unique_instrumentable, len(unique_codes), all_instrumentable, all_contracts))
 
-        block = get_block(i)
+        block = w3.eth.getBlock(i)
 
         transactions = block['transactions']
         for t in transactions:
+            t = w3.eth.getTransaction(t)
+
+            if t is None:
+                continue
+
             addr = t['to']
             if addr is None:
                 continue
 
-            code = get_code(addr)
+            code = w3.eth.getCode(addr)
 
             if code != "0x":
                 all_contracts += 1
@@ -93,10 +61,6 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("end", type=int, help="end block")
-    parser.add_argument("key")
 
     args = parser.parse_args()
-
-    URL += args.key
-
     crawl(args.end)
